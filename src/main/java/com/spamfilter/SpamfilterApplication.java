a3e2df58329cdf7d2fbaa83c3f47db48d5a44942
dhughen93 <dhughen93@gmail.com>
2025-05-05 18:37:32 -0500

    initial java project

---
+++ src/main/java/com/spamfilter/SpamfilterApplication.java
@@ -0,0 +1,13 @@
+package com.spamfilter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpamfilterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpamfilterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spamfilter/SpamfilterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spamfilter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpamfilterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
